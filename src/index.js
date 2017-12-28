f788f47fb0e233913affa66da28346df6b745c98
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2017-12-27 16:59:43 -0800

    fix(api): better inputs
    since we can use mongoose's pluralization rules, we don't need as much
    
    The API requires different inputs now

--- src/index.js
+++ src/index.js
@@ -1,51 +1,48 @@
 import { schema } from 'normalizr';
+import { toCollectionName } from 'mongoose/lib/utils';
 
-export default (schemas, collections) => {
-	const entities = {};
-
-	for (const modelName of Object.keys(schemas)) {
-		const collection = collections[modelName];
-		entities[collection] = new schema.Entity(collection);
-	}
-
-	for (const [modelName, mongooseSchema] of Object.entries(schemas)) {
-		const collection = collections[modelName];
-		const entity = entities[collection];
-
-		const recurse = (tree) => {
-			const obj = {};
-			for (const [key, value] of Object.entries(tree)) {
-				if (value && typeof(value) !== 'object') {
-					continue;
-				}
-				if (!value.ref) {
-					const subObj = recurse(value);
-					if (!subObj) {
-						continue;
-					}
-					obj[key] = Array.isArray(value) ? [subObj[0]] : subObj;
-					continue;
-				}
-				const collection = collections[value.ref];
-				if (!entities[collection]) {
-					continue;
-				}
-				if (value.localField || value.foreignField) {
-					obj[key] = value.justOne ? entities[collection] : [entities[collection]];
-					continue;
-				}
-				obj[key] = entities[collection];
-			}
-			return Object.keys(obj).length && obj;
-		};
-
-		const definition = recurse(mongooseSchema.tree);
-
-		if (!definition) {
+const findRefs = (entities, resources, tree) => {
+	const obj = {};
+	for (const [key, value] of Object.entries(tree)) {
+		if (value && typeof(value) !== 'object') {
 			continue;
 		}
+		if (!value.ref) {
+			const subObj = findRefs(entities, resources, value);
+			if (!subObj) {
+				continue;
+			}
+			obj[key] = Array.isArray(value) ? [subObj[0]] : subObj;
+			continue;
+		}
+		const entity = entities[resources[value.ref].collection];
+		if (!entity) {
+			continue;
+		}
+		if (value.localField || value.foreignField) {
+			obj[key] = value.justOne ? entity : [entity];
+			continue;
+		}
+		obj[key] = entity;
+	}
+	return Object.keys(obj).length && obj;
+};
 
-		entity.define(definition);
+export default (schemas) => {
+	const resources = {};
+	const entities = {};
+
+	for (const [modelName, resource] of Object.entries(schemas)) {
+		resources[modelName] = (resource.constructor === Object) ? { ...resource } : { schema: resource };
+		resources[modelName] = { ...resources[modelName], collection: resources[modelName].collection || toCollectionName(modelName) };
+	}
+
+	for (const resource of Object.values(resources)) {
+		entities[resource.collection] = new schema.Entity(resource.collection);
+	}
+
+	for (const resource of Object.values(resources)) {
+		entities[resource.collection].define(findRefs(entities, resources, resource.schema.tree) || {});
 	}
 
 	return entities;
